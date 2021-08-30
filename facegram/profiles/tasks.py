9959ac246891a1e4d153affb04127bbc65b8d805
Abdullah Adeel <easyautosol@gmail.com>
2021-08-30 14:50:06 +0500

    static files working fine at this moment

--- facegram/profiles/tasks.py
+++ facegram/profiles/tasks.py
@@ -18,8 +18,9 @@ def get_auth_provider_profile_pic(username, pic_url, provider):
     if response.status_code != 200:
         logger.error("Error downloading profile picture for user: %s, status_code=%s" % username, response.status_code)
         return
-    
-    file_name = f"{username}_{provider}_profile"
+        
+    extension = ".png"
+    file_name = f"{username}_{provider}_profile" + extension
     temp_img = tempfile.NamedTemporaryFile(suffix=".jpg", prefix=file_name, delete=False)
 
     for block in response.iter_content(1024 * 8):
