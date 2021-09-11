c5fd387706f8c9925441018ce0f9c945ea8cf662
Abdullah Adeel <easyautosol@gmail.com>
2021-09-11 23:39:26 +0500

    added post upvote model

--- facegram/posts/models.py
+++ facegram/posts/models.py
@@ -4,6 +4,12 @@ from django.contrib.auth import get_user, get_user_model
 
 User = get_user_model()
 
+PRIVACY_CHOICES = (
+    ('OM', "Only Me"),
+    ('OF', "Only Friends"),
+    ('EO', "Every One")
+)
+
 class Post(models.Model):
     author = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='author')
     title = models.CharField(max_length=255)
@@ -12,9 +18,8 @@ class Post(models.Model):
         FileExtensionValidator([
             'png', 'jpg', 'jpeg',
         ])
-    ], blank=True)
-    upvotes = models.IntegerField(default=0)
-    voters = models.ManyToManyField(to=User, related_name='voters')
+    ], blank=True, null=True)
+    privacy = models.CharField(max_length=6, choices=PRIVACY_CHOICES, default='OM')
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
 
@@ -26,3 +31,16 @@ class Post(models.Model):
     class Meta:
         ordering = ('-created_at',)
 
+
+class PostVotes(models.Model):
+
+    REACTION_CHOICES = (
+        ('UV', 'upvote'),
+        ('DV', 'downvote'),
+    )
+
+    post = models.ForeignKey(to=Post, related_name='post', on_delete=models.CASCADE)
+    reaction = models.CharField(max_length=6, choices=REACTION_CHOICES)
+    voters = models.ForeignKey(to=User, related_name='voters')
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
