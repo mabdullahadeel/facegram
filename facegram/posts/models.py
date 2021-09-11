dac05a800b4684e1b3a7079eab1d6b73437c2f55
Abdullah Adeel <easyautosol@gmail.com>
2021-09-11 23:54:56 +0500

    added comments tables

--- facegram/posts/models.py
+++ facegram/posts/models.py
@@ -40,7 +40,24 @@ class PostVotes(models.Model):
     )
 
     post = models.ForeignKey(to=Post, related_name='post', on_delete=models.CASCADE)
+    voter = models.ForeignKey(to=User, related_name='voter', on_delete=models.CASCADE)
     reaction = models.CharField(max_length=6, choices=REACTION_CHOICES)
-    voters = models.ForeignKey(to=User, related_name='voters')
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f"{self.post.id}-{self.voter.username}"
+
+
+
+class PostComment(models.Model):
+    post = models.ForeignKey(to=Post, related_name='related_post', on_delete=models.CASCADE)
+    commenter = models.ForeignKey(to=User, related_name='commenter', on_delete=models.CASCADE)
+    body = models.TextField(max_length=755)
+    total_likes = models.PositiveIntegerField(default=0)
+    likers = models.ManyToManyField(to=User, related_name="likers")
+
+    
+    def __str__(self):
+        return f"{self.post.id}-{self.total_likes}"
