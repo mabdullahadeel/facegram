6d2e1a3e2317ae2067caa602aeeea8f810c6b306
Abdullah Adeel <easyautosol@gmail.com>
2021-10-03 22:28:19 +0500

    update file structure and admin inline models - posts

--- facegram/posts/admin.py
+++ facegram/posts/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
-from facegram.posts.models import Post
+from facegram.posts.models import Post, PostComment, PostVotes
+
+
+class PostCommentInline(admin.StackedInline):
+    model = PostComment
+    extra = 1
+
+
+class PostVotesInline(admin.StackedInline):
+    model = PostVotes
+    extra = 1
 
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'created_at')
     list_filter = ('created_at',)
     search_fields = ('title', 'author__username')
+    inlines = [PostCommentInline, PostVotesInline]
 
 admin.site.register(Post, PostAdmin)
--- facegram/posts/urls.py
+++ facegram/posts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
-from . import views
+from . import posts_view
 
 app_name = "posts_api"
 
 urlpatterns = [
-    path('', views.PostsListView.as_view(), name='get_posts'),
-    path('post/<str:uuid>/', views.PostRetrieveView.as_view(), name='get_post'),
-    path('create/', views.PostCreateView.as_view(), name='create_post'),
-    path('<str:uuid>/', views.PostUpdateDelete.as_view(), name='posts_update_delete'),
+    path('', posts_view.PostsListView.as_view(), name='get_posts'),
+    path('post/<str:uuid>/', posts_view.PostRetrieveView.as_view(), name='get_post'),
+    path('create/', posts_view.PostCreateView.as_view(), name='create_post'),
+    path('<str:uuid>/', posts_view.PostUpdateDelete.as_view(), name='posts_update_delete'),
 ]
